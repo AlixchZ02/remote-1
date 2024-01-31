b2f98e736c0ca84217f67f0ba5daaf5aa39cef64
Ali <i7chzali@gmail.com>
2024-01-31 11:08:17 +0300

    Add random model year funtion

--- cars.py
+++ cars.py
@@ -1,9 +1,25 @@
-x = 4
-a = 1
-while (x<=4):
-	print("Cars")
-	x +=1
+# cars.py
+import random
 
-print("Dodge")
-print("BMW")
-print("Camaro")
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"{self.year} {self.brand} {self.model}")
+
+    def get_random_model_year(self):
+        return random.randint(2000, 2023)
+
+# Creating an instance of Car
+my_car = Car(brand="Toyota", model="Camry", year=2022)
+
+# Displaying car information
+my_car.display_info()
+
+# Getting and displaying a random model year
+random_year = my_car.get_random_model_year()
+print(f"Random Model Year: {random_year}")
+
